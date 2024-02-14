b82c75a89aab9cf575a3e983d86cbda414da643d
burgosfacundo <facundoandresbur@gmail.com>
2024-02-14 19:50:36 -0300

    Add main project

---
+++ apiVinylSound/catalogo/src/main/java/ApiCatalogoApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCatalogoApplication.class, args);
+    }
+}
