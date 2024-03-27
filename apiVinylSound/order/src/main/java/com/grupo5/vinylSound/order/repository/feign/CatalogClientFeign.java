509a3aa32e0df1f0480f2b10a6f11b72c5f5d292
burgosfacundo <facundoandresbur@gmail.com>
2024-03-27 20:51:05 -0300

    Change compose

--- apiVinylSound/order/src/main/java/com/grupo5/vinylSound/order/repository/feign/CatalogClientFeign.java
+++ apiVinylSound/order/src/main/java/com/grupo5/vinylSound/order/repository/feign/CatalogClientFeign.java
@@ -1,13 +1,10 @@
 package com.grupo5.vinylSound.order.repository.feign;
 
-import com.grupo5.vinylSound.order.model.dto.order.ProductOrderResponseDTO;
 import com.grupo5.vinylSound.order.model.dto.product.ProductResponseDTO;
-import com.grupo5.vinylSound.order.model.dto.product.ProductStockRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @FeignClient(name = "catalog",url = "http://localhost:8083")
 public interface CatalogClientFeign {
